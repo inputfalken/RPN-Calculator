258e2b51fcc9c9205bb7b44cf87ed0ceef392b38
Robert Andersson <inputfalken@gmail.com>
2016-09-11 19:16:12 +0200

    rewrote addition method and added a result variable

--- Calculator.cs
+++ Calculator.cs
@@ -3,16 +3,15 @@ using System.Linq;
 namespace ConsoleApplication {
   public class Calculator {
     Queue<int> _queue = new Queue<int>();
+    public int result => _queue.Dequeue();
 
     public Calculator AppendNumber(int num) {
       _queue.Enqueue(num);
       return this;
     }
-    public int Addition() {
-      var num = _queue.Dequeue();
-      while(_queue.Any())
-        num += _queue.Dequeue();
-      return num;
+    public Calculator Addition() {
+        _queue.Enqueue(_queue.Dequeue() + _queue.Dequeue());
+        return this;
     }
 
     public int Subtract() {
@@ -20,6 +19,12 @@ namespace ConsoleApplication {
       while(_queue.Any()) {
         num -= _queue.Dequeue();
       }
+      return num;
+    }
+    public int Times() {
+      var num = _queue.Dequeue();
+      while(_queue.Any())
+        num *= _queue.Dequeue();
       return num;
     }
   }
