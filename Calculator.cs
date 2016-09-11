741f107a5b2a7ff5528989e261bb3a18e788792c
Robert Andersson <inputfalken@gmail.com>
2016-09-12 00:52:43 +0200

    changed from queue to stack and the logic for the calculations

--- Calculator.cs
+++ Calculator.cs
@@ -4,39 +4,43 @@ using System.Linq;
 namespace ConsoleApplication {
   public class Calculator {
 
-    private Queue<int> _queue = new Queue<int>();
+    private Stack<int> Stack {get;}
 
-    public int Result => _queue.Peek();
+    public Calculator(){
+      Stack = new Stack<int>();
+    }
+
+    public int Result => Stack.Peek();
 
     public Token ReadInput(string input) {
       input = input.Trim();
       int value = 0;
       if (int.TryParse(input, out value)){
-        _queue.Enqueue(value);
+        Stack.Push(value);
         return Token.Number;
       }
-      if (_queue.Count >= 2){
+      if (Stack.Count >= 2){
         if (input == "+") {
-          _queue.Enqueue(PerformCalculation((x,y) => x + y));
+          Stack.Push(PerformCalculation((x,y) => x + y));
           return Token.Operator;
         }
         if (input == "-") {
-          _queue.Enqueue(PerformCalculation((x,y) => x - y));
+          Stack.Push(PerformCalculation((x,y) => y - x ));
           return Token.Operator;
         }
         if (input == "*") {
-          _queue.Enqueue(PerformCalculation((x,y) => x * y));
+          Stack.Push(PerformCalculation((x,y) => x * y));
           return Token.Operator;
         }
         if (input == "/") {
-          _queue.Enqueue(PerformCalculation((x,y) => x / y));
+          Stack.Push(PerformCalculation((x,y) => y / x));
           return Token.Operator;
         }
       }
       return Token.Invalid;
     }
     private int PerformCalculation(Func<int,int,int> func){
-      return func(_queue.Dequeue(),_queue.Dequeue());
+      return func(Stack.Pop(),Stack.Pop());
     }
   }
   public enum Token {
