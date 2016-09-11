a75a7c56599cf798f18eb17869aa4056d693155b
Robert Andersson <inputfalken@gmail.com>
2016-09-11 18:19:31 +0200

    removed constructor

--- Calculator.cs
+++ Calculator.cs
@@ -3,9 +3,6 @@ namespace ConsoleApplication {
   public class Calculator {
     Stack<int> _stack = new Stack<int>();
 
-    public Calculator() {
-    }
-
     public Calculator AppendNumber(int num) {
       _stack.Push(num);
       return this;
