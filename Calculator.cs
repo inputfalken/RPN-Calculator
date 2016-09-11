41c4cbe4d86d8834cee20ee058bcc6ea862b32bc
Robert Andersson <inputfalken@gmail.com>
2016-09-11 18:47:35 +0200

    changed from stack to que and added a subtraction method

--- Calculator.cs
+++ Calculator.cs
@@ -2,12 +2,20 @@ using System.Collections.Generic;
 using System.Linq;
 namespace ConsoleApplication {
   public class Calculator {
-    Stack<int> _stack = new Stack<int>();
+    Queue<int> _queue = new Queue<int>();
 
     public Calculator AppendNumber(int num) {
-      _stack.Push(num);
+      _queue.Enqueue(num);
       return this;
     }
-    public int Addition() => _stack.Sum();
+    public int Addition() => _queue.Sum();
+
+    public int Subtract() {
+      var num = _queue.Dequeue();
+      while(_queue.Any()) {
+        num -= _queue.Dequeue();
+      }
+      return num;
+    }
   }
 }
