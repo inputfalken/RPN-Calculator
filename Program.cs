2a1dbd084f8409a02ea70a67d63622bd80f16fe4
Robert Andersson <inputfalken@gmail.com>
2016-09-11 23:34:42 +0200

    added code to run the calculator

--- Program.cs
+++ Program.cs
@@ -1,10 +1,47 @@
 ﻿using System;
 
 namespace ConsoleApplication {
-    public class Program  {
-        public static void Main(string[] args) {
-          var calculator = new Calculator();
-          calculator.AppendNumber(2);
-        }
+  public class Program  {
+    public static void Main(string[] args) {
+      Start();
     }
+
+    private static void Start() {
+      var calculator = new Calculator();
+      Console.WriteLine("Type your Expression");
+      string input = string.Empty;
+
+      while(true) {
+        input = Console.ReadLine();
+        if(input == "calc") break;
+        else {
+          Calculate(input, calculator);
+        }
+      }
+
+      Console.WriteLine(calculator.Result);
+    }
+    private static void Calculate(string input, Calculator calculator) {
+      int value = 0;
+      if (int.TryParse(input, out value)){
+        calculator.AppendNumber(value);
+      }
+      else if (input == "+") {
+        calculator.Addition();
+      }
+      else if (input == "-") {
+        calculator.Subtract();
+      }
+      else if (input == "*") {
+        calculator.Times();
+      }
+      else if (input == "/") {
+        calculator.Divide();
+      }
+      else {
+        Console.WriteLine($"ERROR ON {input}");
+      }
+    }
+  }
 }
+
